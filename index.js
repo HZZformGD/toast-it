125e53a8e9691d0bd4f5ae2a5b0fd367e47683ab
HZZformGD <497923735@qq.com>
2018-03-05 16:39:46 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+var ToastTpl = require('./toast.vue').default; // loader 默认启用 esModule 因此要加default
+var Toast = {}
+Toast.install = (Vue, opts) => {
+    if (document.getElementsByClassName('alertBox').length) {
+        return
+    }
+    let tpl = Vue.extend(ToastTpl)
+    let $vm = new tpl()
+    let mountedTpl = $vm.$mount().$el
+    document.body.appendChild(mountedTpl)
+
+    Vue.prototype.$toast = {
+        show(opts) {
+            if (typeof opts === 'string') {
+                $vm.text = opts
+            }
+            if (typeof opts === 'object') {
+                Object.assign($vm, opts)
+            }
+            $vm.toggleIt({show:true},(e)=> {
+                if (e) {
+                    typeof opts.success == 'function'&& opts.success(e)
+                } else {
+                    typeof opts.error == 'function'&& opts.success(e)
+                }
+            })
+        },
+        hide() {
+            $vm.toggleIt(false)
+        }
+    }
+}
+
+export default Toast
